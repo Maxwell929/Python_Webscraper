50cb4ba96d59095cb3756745edb13c43440a2fc6
Maxwell929 <74867076+Maxwell929@users.noreply.github.com>
2021-03-17 10:45:38 +0100

    Update search_costumization.py

--- scripts/search_costumization.py
+++ scripts/search_costumization.py
@@ -1,11 +1,12 @@
 import time
 from scripts.setup import find_Element, website
 
+#start the website
 website
 
 
 # Function where I select the Element for my search
-# time.sleep is for the loading time, cause somtimes the site is loading and selenium isn't able to find the elements'
+# time.sleep is for the loading time, cause somtimes the site is loading verey slow(bad internet) and selenium isn't able to find the elements'
 def initialization():
 	# Input Beruf and area
 	input_beruf = find_Element('//input[@placeholder="Beruf, Begriff"]')
@@ -44,3 +45,5 @@ def initialization():
 	target.click()
 
 	time.sleep(0.5)
+	
+	# of cours all this can be costumized
