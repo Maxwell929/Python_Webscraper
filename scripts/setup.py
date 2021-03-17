f4ccda4c490b4863b46513795cb052cc058d60b5
Maxwell929 <74867076+Maxwell929@users.noreply.github.com>
2021-03-17 10:43:17 +0100

    Update setup.py

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 
 # Seting up System
+#First you have to download the chromedriver. There are plenty of instructions in the web, so it shouldn't that hard. 
 driver = webdriver.Chrome("/home/maximilian/Downloads/chromedriver")
 website = driver.get("https://www.karriere.at/")
 
+
+#for readable code I realy like to save often used methods... into variables
 find_Element = driver.find_element_by_xpath
 find_Elements = driver.find_elements_by_xpath
 
